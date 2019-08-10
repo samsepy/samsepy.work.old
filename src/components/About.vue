71c16fe319b63a0c7a8cf6a1af531524e3e1d1d5
Hajime Todo <samsepylot@gmail.com>
2019-08-10 12:54:02 +0900

    ページ

--- src/components/About.vue
+++ src/components/About.vue
@@ -1,6 +1,18 @@
 <template>
     <dir class="wrapper">
-        About
+        <h1>Hajime Todo (samsepy)</h1>
+        <p>Software Engineer</p>
+        <h2>できること</h2>
+        <ul>
+            <li>Ruby</li>
+            <li>Ruby on Rails</li>
+            <li>Python</li>
+        </ul>
+        <h2>経歴</h2>
+        <ul>
+            <li>2014年4月~2018年3月 金沢工業大学情報工学部 学部生</li>
+            <li>2018年4月~2019年7月 株式会社オープンエイト サーバサイドエンジニア</li>
+        </ul>
     </dir>
 </template>
 
