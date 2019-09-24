acdd66f9de37e9fb0e3220b0ea53437dc7872eb4
Hajime Todo <samsepylot@gmail.com>
2019-09-24 16:33:36 +0900

    まとめた

--- src/components/Top.vue
+++ src/components/Top.vue
@@ -1,84 +1,47 @@
 <template>
   <div class="top">
-    <h1>{{ msg }}</h1>
-    <h2>Essential Links</h2>
+    <h1>samsepy</h1>
+    <p>1995年8月30日生まれ、24歳。<br>海外で暮らしたい。</p>
+    <h2>Profile</h2>
     <ul>
-      <li>
-        <a
-          href="https://vuejs.org"
-          target="_blank"
-        >
-          Core Docs
-        </a>
-      </li>
-      <li>
-        <a
-          href="https://forum.vuejs.org"
-          target="_blank"
-        >
-          Forum
-        </a>
-      </li>
-      <li>
-        <a
-          href="https://chat.vuejs.org"
-          target="_blank"
-        >
-          Community Chat
-        </a>
-      </li>
-      <li>
-        <a
-          href="https://twitter.com/vuejs"
-          target="_blank"
-        >
-          Twitter
-        </a>
-      </li>
-      <br>
-      <li>
-        <a
-          href="http://vuejs-templates.github.io/webpack/"
-          target="_blank"
-        >
-          Docs for This Template
-        </a>
-      </li>
+      <li>Name: Hajime Todo (東度 基) - samsepy</li>
+      <li>Job: Software Engineer</li>
+      <li>Career:</li>
+      <ul>
+        <li>Kanazawa Institute of Technology (2014-2018) - Bachelor</li>
+        <li>SOUYA Co.,Ltd. (2016-2017) - Part time Engineer</li>
+        <li>OPEN8, Inc. (2018-2019) - Engineer</li>
+        <li>Sole proprietor (2019-) - Engineer</li>
+      </ul>
     </ul>
-    <h2>Ecosystem</h2>
+    <h2>Skills</h2>
     <ul>
-      <li>
-        <a
-          href="http://router.vuejs.org/"
-          target="_blank"
-        >
-          vue-router
-        </a>
-      </li>
-      <li>
-        <a
-          href="http://vuex.vuejs.org/"
-          target="_blank"
-        >
-          vuex
-        </a>
-      </li>
-      <li>
-        <a
-          href="http://vue-loader.vuejs.org/"
-          target="_blank"
-        >
-          vue-loader
-        </a>
-      </li>
-      <li>
-        <a
-          href="https://github.com/vuejs/awesome-vue"
-          target="_blank"
-        >
-          awesome-vue
-        </a>
-      </li>
+      <li>Programming languages: Ruby, Python</li>
+      <li>Frameworks: Ruby on Rails</li>
+      <li>Databases: MySQL</li>
+      <li>Markup languages: HTML5+CSS3</li>
+      <li>Environments: macOS, Git, GitHub</li>
+    </ul>
+    <h2>Sites</h2>
+    <ul>
+      <li><a href="https://github.com/samsepy" target="_blank">GitHub</a></li>
+      <li><a href="http://samsepy.hatenablog.com/" target="_blank">はてなブログ</a></li>
+      <li><a href="https://alis.to/users/samsepy" target="_blank">ALIS</a></li>
+      <li><a href="https://www.facebook.com/hajime.todo" target="_blank">Facebook</a></li>
+      <li><a href="https://www.amazon.jp/hz/wishlist/ls/13WQUGCYG4293?ref_=wl_share" target="_blank">Amazon</a> - wishlist</li>
+    </ul>
+    <h2>Pays</h2>
+    <p>請求や送金する際にこちらを使ってもらうと助かります。</p>
+    <ul>
+      <li>Kyash: @samsepy</li>
+      <li>LINE Pay: @todo1995 - 友達になる必要あり</li>
+      <li>PayPay: @samsepy</li>
+      <li>NEM: NAVLPQ-TXNVHP-GSXUE7-NEH5N4-QMPH2X-Q5VZ65-OWFI</li>
+    </ul>
+    <h2>Contacts</h2>
+    <ul>
+      <li>Mail: <a href="mailto:samsepylot@gmail.com">samsepylot@gmail.com</a></li>
+      <li>Telegram: @samsepy</li>
     </ul>
   </div>
 </template>
@@ -104,7 +67,6 @@ ul {
   padding: 0;
 }
 li {
-  display: inline-block;
   margin: 0 10px;
 }
 a {
