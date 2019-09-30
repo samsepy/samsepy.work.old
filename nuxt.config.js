e7488718af08637ffa1c072551d1d182b1cb8590
Hajime Todo <samsepylot@gmail.com>
2019-10-01 02:16:48 +0900

    renamed

--- nuxt.config.js
+++ nuxt.config.js
@@ -4,7 +4,7 @@ const sourceFileNameToUrl = filepath => {
   const deleteExt = filepath.replace('.md', '')
   const fileName = deleteExt.split('/')[deleteExt.split('/').length - 1]
   const splitArray = fileName.split('-')
-  return `/posts/${splitArray.slice(0, 3).join('-')}/${splitArray.slice(3,).join('-')}`
+  return `/blog/${splitArray.slice(0, 3).join('-')}/${splitArray.slice(3,).join('-')}`
 };
 
 const generateDynamicRoutes = callback => {
