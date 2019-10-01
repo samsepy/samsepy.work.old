3fabb37c5f25a2ff847260c893f361488f93c136
Hajime Todo <samsepylot@gmail.com>
2019-10-01 22:24:55 +0900

    ブログのスタイル設定

--- pages/blog/index.vue
+++ pages/blog/index.vue
@@ -1,3 +1,84 @@
 <template>
-  <div>待ってくれ…</div>
+  <div class="container">
+    <div class="wrapper">
+      <article>
+        <h1>Blog</h1>
+        <article>
+          <h2>gyoe-</h2>
+          <p>ほげええええええええええええええええええええええええええええええええええええええええええええええええええええええええええええええ</p>
+        </article>
+        <article>
+          <h2>poyo-</h2>
+          <p>ぽよおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおお</p>
+        </article>
+      </article>
+    </div>
+  </div>
 </template>
+
+
+<style>
+.container {
+  margin: 0 auto;
+  min-height: 100vh;
+  max-width: 750px;
+}
+
+h1 {
+  margin: 25px 0;
+}
+
+h2 {
+  margin: 20px 0;
+  text-align: left;
+}
+
+h3 {
+  margin: 18px 0;
+  text-align: left;
+}
+
+h1, h2 {
+  color: #96514d;
+  font-weight: normal;
+}
+
+p {
+  margin: 16px 0;
+  text-align: left;
+}
+
+ul {
+  list-style-type: none;
+  padding: 0;
+  text-align: left
+}
+
+li {
+  margin: 5px 0;
+}
+
+a {
+  color: #96514d;
+}
+
+.bio {
+  text-align: center;
+}
+
+.wrapper {
+  padding: 30px 10px;
+}
+
+.logo {
+  border-radius: 50%;
+}
+
+li span {
+  display: inline-block;
+}
+
+.term {
+  color: #96514d;
+}
+</style>
