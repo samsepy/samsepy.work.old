84fba9a91b1050efe4d47764bc4ecf31598d4e17
Hajime Todo <samsepylot@gmail.com>
2019-10-06 03:18:53 +0900

    Revert "nuxt.config.js"
    This reverts commit d6f348639a2fc919c10f0e4387cb4f84982d4edb.

--- nuxt.config.js
+++ nuxt.config.js
@@ -28,8 +28,7 @@ export default {
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
       { hid: 'description', name: 'description', content: '1995年8月30日生まれ、24歳。出身は石川で、現在は東京住まい。海外で暮らしたい。' },
-      { name: 'keywords', content: 'Hajime Todo, samsepy, 東度, 東度基' },
-      { 'http-equiv': 'content-language', content: 'ja' }
+      { name: 'keywords', content: 'Hajime Todo, samsepy, 東度, 東度基' }
     ],
     link: [
       { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
