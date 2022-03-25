011cb3fdd2fae7b7ca6783c9a2ae88b47f13927e
Hajime Todo <samsepylot@gmail.com>
2022-03-25 18:29:51 +0900

    ロケーション更新

--- pages/index.vue
+++ pages/index.vue
@@ -5,7 +5,7 @@
     <p class="bio">
       1995年8月30日生まれ、{{ currentAge }}歳。
       <br />リモートでエンジニアをやっている。 <br />海外で暮らしたい。<a
-        href="https://maps.app.goo.gl/YPWuKsyDXzEfht4r9"
+        href="https://maps.app.goo.gl/UfwrUpCyCRPcLNcEA"
         @click="trackEventClick('location')"
         >津々浦々に出没する</a
       >。
